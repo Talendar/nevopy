002a36ea6ec328e2b593b983cd972af107fa7c6f
Talendar <51515680+Talendar@users.noreply.github.com>
2021-02-12 13:27:42 -0300

    Add wrapper for tf.keras.layers.MaxPool2D

--- nevopy/fixed_topology/layers/__init__.py
+++ nevopy/fixed_topology/layers/__init__.py
@@ -37,3 +37,4 @@ from nevopy.fixed_topology.layers.tf_layers import TensorFlowLayer
 from nevopy.fixed_topology.layers.tf_layers import TFConv2DLayer
 from nevopy.fixed_topology.layers.tf_layers import TFDenseLayer
 from nevopy.fixed_topology.layers.tf_layers import TFFlattenLayer
+from nevopy.fixed_topology.layers.tf_layers import TFMaxPool2DLayer
