576f42412c2ef0f1a4b61e7c492cf9e7df3aa8e5
Talendar <51515680+Talendar@users.noreply.github.com>
2021-02-12 13:22:43 -0300

    Bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ Guedes Nogueira (Talendar).
 from typing import List
 import setuptools
 
-_VERSION = "0.1.0"
+_VERSION = "0.1.1"
 
 # Short description.
 short_description = "An open source neuroevolution framework for Python."
