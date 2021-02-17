904ef775d67c3f942e7d84511472ed7901523648
Talendar <51515680+Talendar@users.noreply.github.com>
2021-02-17 19:48:04 -0300

    Update cart pole example

--- examples/cart_pole/cart_pole_fixed_topology.py
+++ examples/cart_pole/cart_pole_fixed_topology.py
@@ -11,7 +11,6 @@ import os
 
 import gym
 import nevopy as ne
-import numpy as np
 
 
 # Setting this environment variable to "true" will avoid a single process from
@@ -41,16 +40,16 @@ def make_env():
 if __name__ == "__main__":
     # NEvoPy provides a general fitness function that works with most gym
     # environments, so we don't have to worry about implementing it.
-    fitness_function = ne.utils.GymEnvFitness(
+    fitness_function = ne.utils.GymFitnessFunction(
         make_env=make_env,
 
         # Number of "tries" the agent will have in each playing session:
-        num_episodes=5,
+        default_num_episodes=5,
 
         # Since we're using TensorFlow layers, which require inputs in batches,
         # we must change the shape of the input sample before feeding it to our
-        # genomes. The function below does that:
-        pre_process_obs=lambda obs, _: np.reshape(obs, [1, len(obs)]),
+        # genomes. The callback below handles that for us:
+        callbacks=[ne.utils.BatchObsGymCallback()]
     )
 
     # Creating a new random population of fixed-topology genomes:
@@ -83,8 +82,8 @@ if __name__ == "__main__":
     # Now, let's compare the performance of our best genome with the performance
     # of a random agent.
     print("\nEvaluation (25 episodes):")
-    print(f". Random genome score: {fitness_function(None, eps=25)}")
-    print(f". Evolved genome: {fitness_function(best_genome, eps=25)}")
+    print(f". Random genome score: {fitness_function(None, num_eps=25)}")
+    print(f". Evolved genome: {fitness_function(best_genome, num_eps=25)}")
 
     # Visualizing our best genome and a random agent playing:
     while True:
@@ -95,10 +94,10 @@ if __name__ == "__main__":
                         "Choose an option: "))
         if key == 0:
             print(f"\nRandom genome: "
-                  f"{fitness_function(None, eps=1, visualize=True)}")
+                  f"{fitness_function(None, num_eps=1, visualize=True)}")
         elif key == 1:
             print(f"\nEvolved genome: "
-                  f"{fitness_function(best_genome, eps=1, visualize=True)}")
+                  f"{fitness_function(best_genome, num_eps=1, visualize=True)}")
         elif key == 2:
             break
         else:
